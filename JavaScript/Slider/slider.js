7bd21ce66a30682617cbae17dc4a26bbf6af5279
elephant2601 <elephant2601@gmail.com>
2016-06-28 17:37:21 +0700

    100%

--- JavaScript/Slider/slider.js
+++ JavaScript/Slider/slider.js
@@ -1,25 +1,29 @@
-﻿/*
-lanclick = 0; click prev 1 cai -> lanclick--
-click next 1 cai -> lanclick++
-pos0=-851px, pos1=0px, pos2=851px
-bt img-1 pos1, img-2345 pos2
-onclick + next + lanclick=2 img-1 pos0, img2 pos1 , img-345 pos3
-onclick + prev + lanclick=
-*/
-var numberClick = 1;
-function previous() {
-	if (numberClick == 1) {
-		break;
-	}
-	else {
-		numberClick--;
-	}
+﻿/* Slider.js */
+
+var slideCurrent = 1;
+showImage(slideCurrent);
+
+function prev_next(n) {
+	showImage(slideCurrent += n);
 }
-function next() {
-	if (numberClick == 5) {
-		break;
+
+function numberClick(n) {
+	showImage(slideCurrent = n);
+}
+
+function showImage(n) {
+	var i;
+	var x = document.getElementsByClassName("image");
+	if (n > x.length) {
+		slideCurrent = 1;
 	}
-	else {
-		numberClick++;
+	if (n < 1) {
+		slideCurrent = x.length;
 	}
+	//hidden all image
+	for ( i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}	
+	//show 1 image
+	x[slideCurrent-1].style.display = "block";
 }
