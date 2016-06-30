dcf811039bc7da66ef8e6dabeb6dd514bae8d932
elephant2601 <elephant2601@gmail.com>
2016-06-30 18:13:28 +0700

    update

--- JavaScript/Slider/slider.js
+++ JavaScript/Slider/slider.js
@@ -1,7 +1,10 @@
 ﻿/* Slider.js */
 
-var slideCurrent = 1;
-showImage(slideCurrent);
+var slideCurrent = 0;
+var n;
+var i;
+var x = document.getElementsByClassName("image");
+var y = document.getElementsByClassName("iconImage");
 
 function prev_next(n) {
 	showImage(slideCurrent += n);
@@ -12,8 +15,7 @@ function numberClick(n) {
 }
 
 function showImage(n) {
-	var i;
-	var x = document.getElementsByClassName("image");
+	//back when the final pass
 	if (n > x.length) {
 		slideCurrent = 1;
 	}
@@ -23,7 +25,36 @@ function showImage(n) {
 	//hidden all image
 	for ( i = 0; i < x.length; i++) {
 		x[i].style.display = "none";
-	}	
-	//show 1 image
+	}
+	//hidden all image-border
+	for ( i = 0; i < y.length; i++) {
+		y[i].style.border = "0px";
+	}
+	//show 1 image and image-border
 	x[slideCurrent-1].style.display = "block";
+	y[slideCurrent-1].style.border = "2px solid #f44336";
+
 }
+
+function autoNextImage() {
+	slideCurrent++;
+	//back when the final pass
+	if (slideCurrent > x.length) {
+		slideCurrent = 1;
+	}
+	if (slideCurrent < 1) {
+		slideCurrent = x.length;
+	}
+	//hidden all image
+	for ( i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	//hidden all image-border
+	for ( i = 0; i < y.length; i++) {
+		y[i].style.border = "0px";
+	}
+	//show 1 image and image-border
+	x[slideCurrent-1].style.display = "block";
+	y[slideCurrent-1].style.border = "2px solid #f44336";
+}
+setInterval(autoNextImage, 5000);
