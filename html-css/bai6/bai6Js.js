b7a17bc9aa79d7c839e506c2dae48b6ec123fbed
elephant2601 <elephant2601@gmail.com>
2016-06-30 13:51:33 +0700

    update comment

--- html-css/bai6/bai6Js.js
+++ html-css/bai6/bai6Js.js
@@ -1,10 +1,16 @@
-﻿/* Javascript */
+﻿/* Javascript
+hide or show the content when onclick lauch
+image change when onlick lauch
+variable i corresponds to the order content
+*/
 function showHideinfo(i) {
 	var display = document.getElementById("thehouse" + i).style.display;
+	//the content hidden when click
 	if(display != "none") {
 		document.getElementById("arrow" + i).style.backgroundPosition = "2px -19px";
 		document.getElementById("thehouse" + i).style.display = "none";
 	}
+	//the content show when click
 	else {
 		document.getElementById("arrow" + i).style.backgroundPosition="0px 6px";
 		document.getElementById("thehouse" + i).style.display = "block";
