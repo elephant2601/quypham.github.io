c171623f1ac9185950167b24514967b3f7c8f8b7
elephant2601 <elephant2601@gmail.com>
2016-06-30 11:21:37 +0700

    update

---
+++ html-css/bai6/bai6Js.js
@@ -0,0 +1,12 @@
+﻿/* Javascript */
+function showHideinfo(i) {
+	var display = document.getElementById("thehouse" + i).style.display;
+	if(display != "none") {
+		document.getElementById("arrow" + i).style.backgroundPosition = "2px -19px";
+		document.getElementById("thehouse" + i).style.display = "none";
+	}
+	else {
+		document.getElementById("arrow" + i).style.backgroundPosition="0px 6px";
+		document.getElementById("thehouse" + i).style.display = "block";
+	}
+}
