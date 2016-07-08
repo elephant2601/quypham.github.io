2a00ef237a096a9deaf41f0e8dee40baf344e44b
elephant2601 <elephant2601@gmail.com>
2016-07-08 12:07:36 +0700

    update

--- JavaScript/formAjax/calendarJs.js
+++ JavaScript/formAjax/calendarJs.js
@@ -2,7 +2,7 @@
 
 var date = ["Sun", "Mon", "Tue", "Web", "Thu", "Fri", "Sat"];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var dayLastMonth;
+var dayLastMonth, content;
 var dayArr = [];
 var numDayNowMonth;// This month's dates
 var numDayLastWeek; // Last week's number of days last month
@@ -12,6 +12,8 @@ var monthInput = today.getMonth();
 var dayInput = today.getDate();
 var yearInput = today.getFullYear();
 
+initCalendar();
+
 //create calendar
 function initCalendar() {
 	varloop = 0;
@@ -19,45 +21,56 @@ function initCalendar() {
 	numDayLastWeek = new Date(yearInput, monthInput, 1).getDay();
 	// taking the number of days this month
 	numDayNowMonth = new Date(yearInput, monthInput + 1, 0).getDate();
-	document.write("<div id = 'main'>");
-	document.write("<input id = 'showCalendar' style = 'height: 36px; position: relative;'><img  onclick = 'openCalendar()' src = 'calendar.png' alt = '' style = 'position: absolute; top: 10px; left: 146px;'>");
-	document.write("<table id = 'calendar' style = 'border: 1px solid black; text-align: center; background-color: #2D2D2D'>");
+	content = "<div id = 'main'>";
+	content +="<input id = 'showCalendar' style = 'height: 32px; position: relative;'><img  onclick = 'openCalendar()' src = 'calendar.png' alt = '' style = 'position: absolute; top: 89px; left: 222px;'>";
+	content += "<table id = 'calendar' style = 'border: 1px solid black; text-align: center; background-color: #2D2D2D'>";
 	optionMonthYear();
 	setCalendar();
-	document.write("</table>");
-	document.write("</div>");
+	content += "</table>";
+	content += "</div>";
+	document.getElementById('demo').innerHTML = content;
 	document.getElementById("selectMonth").value = monthInput;
 	document.getElementById("selectYear").value = yearInput;
 }
 
 //create optional month and year
 function optionMonthYear() {
-	document.write("<tr>");
-	document.write("<td class = 'click' id = 'prevYear' onclick = 'prevYear()' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px; cursor: pointer;'>&#8647</td>");
-	document.write("<td class = 'click' id = 'prevMonth' onclick = 'prevMonth()' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px; cursor: pointer;'>&larr;</td>");
+	content += "<tr>";
+	content += "<td class = 'click' id = 'prevYear' onclick = 'prevYear()' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px; cursor: pointer;'>&#8647</td>";
+	content += "<td class = 'click' id = 'prevMonth' onclick = 'prevMonth()' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px; cursor: pointer;'>&larr;</td>";
 	buildMonth();
 	buildYear();
-	document.write("<td class = 'click' id = 'nextMonth' onclick = 'nextMonth()' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px; cursor: pointer;'>&rarr;</td>");
-	document.write("<td class = 'click' id = 'nextYear' onclick = 'nextYear()' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px; cursor: pointer;'>&#8649;</td>");
-	document.write("</tr>");
+	content += "<td class = 'click' id = 'nextMonth' onclick = 'nextMonth()' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px; cursor: pointer;'>&rarr;</td>";
+	content += "<td class = 'click' id = 'nextYear' onclick = 'nextYear()' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px; cursor: pointer;'>&#8649;</td>";
+	content += "</tr>";
 }
 
 //create optional month
 function buildMonth() {
-	document.write("<td class = 'click' colspan = '2' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px;'><select id = 'selectMonth' onchange='changeMonth();'>");
+	content += "<td class = 'click' colspan = '2' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px;'><select id = 'selectMonth' onchange='changeMonth()'>";
 	for (i = 0; i < 12; i++) {
-		document.write("<option value ='" + i + "'>" + month[i] + "</option>");
+		content += "<option value ='";
+		content += i;
+		content += "'>";
+		content += month[i];
+		content += "</option>";
 	};
-	document.write("</select>" + "</td>");
+	content += "</select>";
+	content += "</td>";
 }
 
 //creat optional year
 function buildYear() {
-	document.write("<td class = 'click' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px;'><select id = 'selectYear' onchange='changeYear()'>");
-	for (y = 1970; y <= 2100; y++) {
-		document.write("<option value ='" + y + "'>" + y + "</option>");
+	content += "<td class = 'click' style = 'background-color: #a9cce3; color: #2e86c1; border: 1px solid black; width: 61px; height: 33px;'><select id = 'selectYear' onchange='changeYear()'>";
+	for (i = 1970; i <= 2100; i++) {
+		content += "<option value ='";
+		content += i;
+		content += "'>";
+		content += i;
+		content += "</option>";
 	};
-	document.write("</select>" + "</td>");
+	content += "</select>";
+	content += "</td>";
 }
 
 // create row and column, fill the number day
@@ -68,36 +81,47 @@ function setCalendar() {
 	}
 
 	//drawing table calendar (day)
-	document.write("<tr>");
+	content += "<tr>";
 	for (i = 0; i < 7; i++) {
-		document.write("<td style = 'border: 1px solid black; width: 61px; height: 33px; background-color: #BEBEBE; color: red; font-weight: bold; font-size: 18pt;'>" + date[i] + "</td>");
+		content += "<td style = 'border: 1px solid black; width: 61px; height: 33px; background-color: #BEBEBE; color: red; font-weight: bold; font-size: 18pt;'>";
+		content += date[i];
+		content += "</td>";
 	}
-	document.write("</tr>");
+	content += "</tr>";
 
 	//drawing table calendar (date)
 	for (x = 0; x < 6; x++) {
-		document.write("<tr>");
+		content += "<tr>";
 		for (i = 0; i < 7; i++) {
 			if (varloop < numDayNowMonth) {
 				//drawing the number of days the last week of the previous month
 				if (numDayLastWeek > 0) {
-					document.write("<td style = 'border: 1px solid black; width: 61px; height: 33px;'>" + "</td>");
+					content += "<td style = 'border: 1px solid black; width: 61px; height: 33px;'>";
+					content += "</td>";
 					numDayLastWeek--;
 				}
 				//drawing the number of days this month
 				else {
-					//highlight today
+					//hightlight today
 					if ((dayArr[varloop] == dayInput) && (monthInput == today.getMonth()) && (yearInput == today.getFullYear())) {
-						document.write("<td style = 'border: 1px solid black; width: 61px; height: 33px; color: red; font-weight: bold; background-color: white; cursor: pointer;' onclick='selectDay(" + varloop + ")'>" + dayArr[varloop] + "</td>");
+						content += "<td style = 'border: 1px solid black; width: 61px; height: 33px; color: red; font-weight: bold; background-color: white; cursor: pointer;' onclick='selectDay(";
+						content += varloop;
+						content += ")'>";
+						content += dayArr[varloop];
+						content += "</td>";
 					}
 					else {
-					document.write("<td style = 'border: 1px solid black; width: 61px; height: 33px; background-color: white; cursor: pointer;' onclick='selectDay(" + varloop + ")'>" + dayArr[varloop] + "</td>");
+					content += "<td style = 'border: 1px solid black; width: 61px; height: 33px; background-color: white; cursor: pointer;' onclick='selectDay(";
+					content += varloop;
+					content += ")'>";
+					content += dayArr[varloop];
+					content += "</td>";
 					}
 					varloop++;
 				}
 			}
 		}
-		document.write("</tr>");
+		content += "</tr>";
 	}
 }
 
@@ -106,6 +130,7 @@ function prevYear() {
 	clearCalendar();
 	yearInput--;
 	initCalendar();
+	document.getElementById('calendar').style.display = 'block';
 }
 
 //previous month button
@@ -117,6 +142,7 @@ function prevMonth() {
 		yearInput--;
 	}
 	initCalendar();
+	document.getElementById('calendar').style.display = 'block';
 }
 
 //next month button
@@ -128,6 +154,7 @@ function nextMonth() {
 		yearInput++;
 	}
 	initCalendar();
+	document.getElementById('calendar').style.display = 'block';
 }
 
 //next year button
@@ -135,22 +162,23 @@ function nextYear() {
 	clearCalendar();
 	yearInput++;
 	initCalendar();
+	document.getElementById('calendar').style.display = 'block';
 }
 
 //change month
-function changeMonth() {
-	//clearCalendar();
+function changeMonth() {	
 	monthInput = document.getElementById("selectMonth").selectedIndex;
 	clearCalendar();
 	initCalendar();
+	document.getElementById('calendar').style.display = 'block';
 }
 
 //change year
 function changeYear() {
-	//clearCalendar();
 	yearInput = document.getElementById("selectYear").selectedIndex + 1970;
 	clearCalendar();
 	initCalendar();
+	document.getElementById('calendar').style.display = 'block';
 }
 
 //select the day and show up input
@@ -169,6 +197,6 @@ function openCalendar() {
 
 //clear calendar in order to create new calendar
 function clearCalendar() {
-	var item = document.getElementById("main");
-	item.parentNode.removeChild(item);
+	document.getElementById('showCalendar').style.display = 'none';
+	document.getElementById('calendar').style.display = 'none';
 }
