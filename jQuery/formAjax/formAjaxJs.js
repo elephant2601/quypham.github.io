c2e444eb90fb39d6c8b5cb01a96c7eda6a951791
elephant2601 <elephant2601@gmail.com>
2016-07-29 00:44:24 +0700

    update

--- jQuery/formAjax/formAjaxJs.js
+++ jQuery/formAjax/formAjaxJs.js
@@ -1,150 +1,197 @@
-﻿var submit = 0;
+var submit = 0;
+var alertString, idAlert;
+/*var username = $('#username').val();
+var password = $('#password').val();
+var email = $('#email').val();
+var birthday = $('#demo').val();*/
 
 //check the characters of username
 function  checkUsername() {
-	var usernameLen = document.getElementById("username").value.length;
-	if (usernameLen == 0) {
-		document.getElementById("alertUserName").innerHTML = "Empty username";
-		document.getElementById("alertUserName").style.color = "#f44336";
-	}
-	else {
-		if (usernameLen < 8) {
-		document.getElementById("alertUserName").innerHTML = "Username length min 8 letter";
-		document.getElementById("alertUserName").style.color = "#f44336";
-		}
-		else {
-			check();
-		}	
-	}
+    var usernameLen = document.getElementById("username").value.length;
+    if (usernameLen == 0) {
+        alertString = "Empty username";
+    }
+    else {
+        if (usernameLen < 8) {
+            alertString = "Username length min 8 letter";
+        }
+        else {
+        	alertString = "OK";
+            //check();
+        }   
+    }
+    idAlert = "alertUserName";
+    alertAll();
 }
 
 //check the characters of password
 function checkPassword() {
-	var passwordLen = document.getElementById("password").value.length;
+    var passwordLen = document.getElementById("password").value.length;
 
-	if (passwordLen == 0) {
-		document.getElementById("alertPassword").innerHTML = "Empty password";
-		document.getElementById("alertPassword").style.color = "#f44336";
-	}
-	else {
-		if (passwordLen < 8) {
-			document.getElementById("alertPassword").innerHTML = "Password length min 8 letter";
-			document.getElementById("alertPassword").style.color = "#f44336";
-		}
-		else {
-			document.getElementById("alertPassword").innerHTML = "OK";
-			document.getElementById("alertPassword").style.color = "#039be5";
-			submit++;
-		}
-	}
+    if (passwordLen == 0) {
+        alertString = "Empty password";
+    }
+    else {
+        if (passwordLen < 8) {
+            alertString = "Password length min 8 letter";
+        }
+        else {
+            alertString = "OK";
+            submit++;
+        }
+    }
+    idAlert = "alertPassword";
+    alertAll();
 }
 
 //check the characters of email
 function checkEmail() {
-	var email, emailArr, emailLen;
-	var counter = 0;
+    var email, emailArr, emailLen;
+    var counter = 0;
 
-	email = document.getElementById("email").value;
-	emailLen = email.length;
-	emailArr = email.split("");
+    email = document.getElementById("email").value;
+    emailLen = email.length;
+    emailArr = email.split("");
 
-	for (i = 0; i < emailArr.length; i++) {
-		if (emailArr[i] == "@" || emailArr[i] == ".") {
-			counter++;
-		}
-	}
+    for (i = 0; i < emailArr.length; i++) {
+        if (emailArr[i] == "@" || emailArr[i] == ".") {
+            counter++;
+        }
+    }
 
-	if(emailLen == 0) {
-		document.getElementById("alertEmail").innerHTML = "Empty email";
-		document.getElementById("alertEmail").style.color = "#f44336";
-	}
-	else {
-		if (counter == 2) {
-			document.getElementById("alertEmail").innerHTML = "OK";
-			document.getElementById("alertEmail").style.color = "#039be5";
-			submit++;
-		}
-		else {
-			document.getElementById("alertEmail").innerHTML = "Email wrong format";
-			document.getElementById("alertEmail").style.color = "#f44336";
-		}
-	}
+    if(emailLen == 0) {
+        alertString = "Empty email";
+    }
+    else {
+        if (counter == 2) {
+            alertString = "OK";
+            submit++;
+        }
+        else {
+            alertString = "Email wrong format";
+        }
+    }
+    idAlert = "alertEmail";
+    alertAll();
 }
 
 //check after entering birthday
 function checkBirthday1() {
-	checkBirthday();
+    checkBirthday();
 }
 
 //check after selecting birthday
 function checkBirthday2() {
-	checkBirthday();
+    checkBirthday();
 }
 
 function checkBirthday() {
-	var checkBir = document.getElementById("showCalendar").value;
-	var checkBirArr = checkBir.split("");
+    var checkBir = document.getElementById("showCalendar").value;
+    var checkBirArr = checkBir.split("");
 
-	for (i = 0; i < checkBirArr.length; i++) {
-		if (checkBirArr[i] == "/") {
-			document.getElementById("alertBirthday").innerHTML = "OK";
-			document.getElementById("alertBirthday").style.color = "#039be5";
-			submit++;
-			break;
-		}
-		else {
-			document.getElementById("alertBirthday").innerHTML = "Birthday wrong format";
-			document.getElementById("alertBirthday").style.color = "#f44336";
-		}
-	}
+    for (i = 0; i < checkBirArr.length; i++) {
+        if (checkBirArr[i] == "/") {
+            alertString = "OK";
+            submit++;
+            break;
+        }
+        else {
+            alertString = "Birthday wrong format";
+        }
+    }
+    idAlert = "alertBirthday";
+    alertAll();
 }
 
 //submit form
 function submitAll() {
-	if (submit == 4) {
-		alert("Submit successfully!")
-		submit = 0;
-	}
-	else {
-		alert("Please fill again!")
-	}
+    /*if (submit == 4) {
+        alert("Submit successfully!");
+        submit = 0;
+    }
+    else {
+        alert("Please fill again!");
+    }*/
+    $.ajax ({
+    	url : 'formAjaxPHP.php',
+    	type : 'post',
+    	dataType : 'text',
+    	data : {
+            /*username : username,
+            password : password,
+    		email : email,
+    		birthday : birthday*/
+    		username : $('#username').val(),
+			password : $('#password').val(),
+			email : $('#email').val(),
+			birthday : $('#demo').val()
+    		
+		},
+		success : function(result) {
+            if (!result.hasOwnProperty('error') || result['error'] != 'success') {
+                alert('unsuccess');
+            }
+			console.log(result.username);
+			/*if($.trim(result.username) != '') {
+			 	alertString = result.username;
+			 	idAlert = "alertUserName";
+			 	alertAll();
+			 }
+    		 if($.trim(result.email) != '') {
+    		 	alertString = result.email;
+    		 	idAlert = "alertEmail";
+    		 	alertAll();
+    		 }*/
+		}
+    });
 }
 
 //refresh form
 function refreshAll() {
-	document.getElementById("alertUserName").innerHTML = null;
-	document.getElementById("alertPassword").innerHTML = null;
-	document.getElementById("alertEmail").innerHTML = null;
-	document.getElementById("alertBirthday").innerHTML = null;
-	document.getElementById("myForm").reset();
-	submit = 0;
+    document.getElementById("alertUserName").innerHTML = null;
+    document.getElementById("alertPassword").innerHTML = null;
+    document.getElementById("alertEmail").innerHTML = null;
+    document.getElementById("alertBirthday").innerHTML = null;
+    document.getElementById("myForm").reset();
+    submit = 0;
 }
 
 //check valid username
-function check() {
-	var checkUser, result;
+/*function check() {
+    var checkUser, result;
 
-	if(window.XMLHttpRequest) {
-		checkUser = new XMLHttpRequest;
-	}
-	else {
-		checkUser = new ActiveXObject("Microsoft.XMLHTTP");
-	}
+    if(window.XMLHttpRequest) {
+        checkUser = new XMLHttpRequest;
+    }
+    else {
+        checkUser = new ActiveXObject("Microsoft.XMLHTTP");
+    }
 
-	checkUser.onreadystatechange = function() {
-		if (checkUser.readyState == 4 && checkUser.status == 200) {
-			result = checkUser.responseText;
-			document.getElementById("alertUserName").innerHTML = result;
-			if (result == "OK") {
-				document.getElementById("alertUserName").style.color = "#039be5";
-				submit++;
-			}
-			else {
-				document.getElementById("alertUserName").style.color = "#f44336";
-			}
-		}
-	}
-	var usern = document.getElementById("username").value;
-	checkUser.open("GET", "formAjaxPHP.php?username=" + usern, true);
-	checkUser.send();
+    var usern = document.getElementById("username").value;
+    checkUser.open("GET", "formAjaxPHP.php?username=" + usern, true);
+    checkUser.send();
+
+    checkUser.onreadystatechange = function() {
+        if (checkUser.readyState == 4 && checkUser.status == 200) {
+            result = checkUser.responseText;
+            alertString = result;
+            idAlert = "alertUserName";
+            alertAll();
+            if (result == "OK") {
+                submit++;
+            }
+        }
+    }
+}*/
+
+//alert after entering
+function alertAll() {
+    document.getElementById(idAlert).innerHTML = alertString;
+    if (alertString == "OK") {
+        document.getElementById(idAlert).style.color = "#039be5";
+    }
+    else {
+        document.getElementById(idAlert).style.color = "#f44336";
+    }
+    alertString = null;
 }
--- jQuery/formAjax/formAjaxPHP.php
+++ jQuery/formAjax/formAjaxPHP.php
@@ -1,59 +1,58 @@
-
-
-/*if(isset($_REQUEST["username"])) {
-    $username = array("elephant", "elephant2601");
-        if(in_array($_REQUEST["username"], $username)) {
-            echo 'Username already exists';
-        }
-        else {
-            echo 'OK';
-        }
-}
-else {
-    echo 'NULL';
-}*/
-
-
 <?php
     $username = isset($_POST['username']) ? $_POST['username'] : false;
+    $password = isset($_POST['password']) ? $_POST['password'] : false;
     $email = isset($_POST['email']) ? $_POST['email'] : false;
+    $birthday = isset($_POST['birthday']) ? $_POST['birthday'] : false;
+
+
 
     //connect database
-    $conn = mysqli_connect('sql208.freevnn.com', 'freev_18514815', 'quy2601', 'freev_18514815_formAjax') or die ('{error:"bad_request"}');
+    //$conn = mysqli_connect('sql208.freevnn.com', 'freev_18514815', 'quy2601', 'freev_18514815_formAjax') or die ('{error:"bad_request"}');
+    $conn = mysqli_connect('localhost', 'root', 'vertrigo', 'formajax') or die ('{error:"bad_request"}');
+
+    // Khai báo biến lưu lỗi
+    $error = array(
+        'error' => 'success',
+        'password' => '',
+        'username' => '',
+        'email' => '',
+        'birthday' => ''
+    );
 
     // check username
-    if ($username)
-    {
-        $query = mysqli_query($conn, 'select count(*) as count from formAjax where USERNAME = \''.  addslashes($username).'\'');
-     
-        if ($query){
-            $row = mysqli_fetch_array($query, MYSQLI_ASSOC);
-            if ((int)$row['count'] > 0){
-                echo 'Username already exists';
-            }
-            else {
-                echo 'OK';
-            }
-        }
-    }
-     
-    // check mail
-    if ($email) {
-        $query = mysqli_query($conn, 'select count(*) as count from formAjax where EMAIL = \''.  addslashes($email).'\'');     
+    if ($username) {
+        $query = mysqli_query($conn, "SELECT `USERNAME` FROM `formajax` WHERE USERNAME = $username");
+        //$query = mysqli_query($conn, 'select * from formajax where USERNAME = $username');
         if ($query) {
             $row = mysqli_fetch_array($query, MYSQLI_ASSOC);
-            if ((int)$row['count'] > 0) {
-                echo 'Email already exists';
+            if ( count($row) > 0){
+                $error['username'] = 'Username already exists';
             }
             else {
-                echo 'OK';
+                $error['username'] = 'OK';
+            }
+        }
+    }
+
+    // check mail
+    if ($email) {
+        //$query = mysqli_query($conn, 'select count(*) as count from formajax where EMAIL = \''.  addslashes($email).'\'');
+        $query = mysqli_query($conn, "SELECT `EMAIL` FROM `formajax` WHERE EMAIL = $email");
+        if ($query) {
+            $row = mysqli_fetch_array($query, MYSQLI_ASSOC);
+            if ( count($row) > 0) {
+                $error['email'] = 'Email already exists';
+            }
+            else {
+                $error['email'] = 'OK';
             }
         }
     }
      
-    if (!$error['username'] && !$error['email']){
+    if (!$error['username'] && !$error['email'] && !$error['password'] && !$error['birthday']) {
         // insert into database if no error
-        $query = mysqli_query($conn, "insert into member(username, email) value ('$username','$email')");
+        //$query = mysqli_query($conn, "insert into formajax(USERNAME, PASSWORD, EMAIL, BIRTHDAY) values ('$username', '$password', '$email', '$birthday')");
+        $query = mysqli_query($conn, "INSERT INTO `formajax`(`USERNAME`, `PASSWORD`, `EMAIL`, `BIRTHDAY`) VALUES ('$username', '$password', '$email', '$birthday')");
     }
-    die (json_encode($error));
+    die ($error);
 ?>
